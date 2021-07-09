f7761585d8321483d3560d7bcb3678abefa296e2
Jakob Gaard Andersen <jakobsky@live.dk>
2021-07-09 12:47:58 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+const nextConfig = {
+  reactStrictMode: true,
+  future: {},
+  experimental: {},
+}
+
+module.exports = nextConfig;
